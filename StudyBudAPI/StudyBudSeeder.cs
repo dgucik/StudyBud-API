77623e7e26c6279555b2b36dffeeb58e75585aa1
danielsg <danielgucik7@gmail.com>
2023-12-14 22:10:45 +0100

    Create database seeder

---
+++ StudyBudAPI/StudyBudSeeder.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyBudAPI.Entities;
+
+namespace StudyBudAPI
+{
+	public class StudyBudSeeder
+	{
+		private readonly StudyBudDbContext _dbContext;
+
+		public StudyBudSeeder(StudyBudDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public void Seed()
+		{
+			if (_dbContext.Database.CanConnect())
+			{
+				var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+				if(pendingMigrations != null && pendingMigrations.Any())
+				{
+					_dbContext.Database.Migrate();
+				}
+
+				if (!_dbContext.Topics.Any())
+				{
+					var topics = GetTopics();
+					_dbContext.Topics.AddRange(topics);
+					_dbContext.SaveChanges();
+				}
+			}
+		}
+
+		private List<Topic> GetTopics()
+		{
+			return new List<Topic> {
+				new Topic()
+				{
+					Name = "Sport"
+				},
+				new Topic()
+				{
+					Name = "Politics"
+				},
+				new Topic()
+				{
+					Name = "Films"
+				}
+			};
+		}
+	}
+}
