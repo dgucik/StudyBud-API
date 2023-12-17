fd2f7a36483e6b2d5e8f098f4188fab65fa2e76b
danielsg <danielgucik7@gmail.com>
2023-12-17 20:47:42 +0100

    Create paginated result

---
+++ StudyBudAPI/Models/Validator/StudyBudQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace StudyBudAPI.Models.Validator
+{
+	public class StudyBudQueryValidator : AbstractValidator<StudyBudQuery>
+	{
+		private int[] allowedPageSize = { 5, 10, 15 };
+
+		public StudyBudQueryValidator()
+		{
+			RuleFor(r => r.PageNumber)
+				.GreaterThanOrEqualTo(1);
+
+			RuleFor(r => r.PageSize)
+				.Custom((value, context) =>
+				{
+					if (!allowedPageSize.Contains(value))
+					{
+						context.AddFailure(nameof(StudyBudQuery.PageSize), $"Page size must be in [{string.Join(", ", allowedPageSize)}]");
+					}
+				});
+		}
+	}
+}
